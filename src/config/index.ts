9cd37313f85fe05b90a71001242f95f4adc56625
job25721 <amashiikaze@gmail.com>
2021-05-24 10:26:28 +0700

    update env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,10 @@ dotenv.config()
 const NODE_PORT = process.env.PORT || 8080
 const API_PREFIX = process.env.API_PREFIX || '/api'
 const mongoConfig = {
-  user: process.env.MONGO_USER || '',
-  password: process.env.MONGO_PASS || '',
-  db: process.env.MONGO_DB || '',
-  mongoURL: process.env.MONGO_URL || '',
+  user: process.env.MONGO_USER,
+  password: process.env.MONGO_PASS,
+  db: process.env.MONGO_DB,
+  mongoURL: process.env.MONGO_URL || 'mongo://localhost:27017',
   ssl: Boolean(process.env.MONGO_URL) || false,
 }
 export default {
